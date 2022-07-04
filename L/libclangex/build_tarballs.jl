a5402e5acacaaa962eeb6b7ecbfc1e9b9baaa4ae
Valentin Churavy <v.churavy@gmail.com>
2022-07-03 20:30:56 -0400

    support multiple LLVM version

--- L/libclangex/build_tarballs.jl
+++ L/libclangex/build_tarballs.jl
@@ -10,11 +10,11 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 name = "libclangex"
 version = v"0.1.6"
 
-llvm_versions = [#=v"11.0.1",=# v"12.0.1", v"13.0.1", v"14.0.2"]
+llvm_versions = [#=v"11.0.1",=# v"12.0.1", v"13.0.1", #=v"14.0.2"=#]
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/Gnimuc/libclangex.git", "527f22f606f43682c89ef76746d5d9652b2db1dc")
+    GitSource("https://github.com/Gnimuc/libclangex.git", "0b42d5e72a406e18421d4bd8e79414c56d0bbe3b")
 ]
 
 # Bash recipe for building across all platforms
@@ -23,9 +23,10 @@ cd $WORKSPACE/srcdir
 cd libclangex/
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-     -DLLVM_DIR=$prefix \
-     -DCLANG_DIR=$prefix \
-     -DLLVM_ASSERT_BUILD=false \
+     -DLLVM_DIR=${prefix} \
+     -DCLANG_DIR=${prefix} \
+     -DLLVM_ASSERT_BUILD=${LLVM_ASSERTIONS} \
+     -DLLVM_VERSION_MAJOR=${LLVM_MAJ_VER}
      -DCMAKE_BUILD_TYPE=Release \
      -DCMAKE_EXPORT_COMPILE_COMMANDS=true
 make -j${nproc}
@@ -52,8 +53,14 @@ for llvm_version in llvm_versions, llvm_assertions in (false, true)
     # Dependencies that must be installed before this package can be built
     llvm_name = llvm_assertions ? "LLVM_full_assert_jll" : "LLVM_full_jll"
     dependencies = [
+        RuntimeDependency("Clang_jll"),
         BuildDependency(PackageSpec(name=llvm_name, version=llvm_version))
     ]
+
+    env = """
+    LLVM_MAJ_VER=$(llvm_versions.major)
+    LLVM_ASSERTIONS=$(LLVM_ASSERTIONS)
+    """
 
     # The products that we will ensure are always built
     products = Product[
@@ -71,6 +78,7 @@ for llvm_version in llvm_versions, llvm_assertions in (false, true)
         push!(builds, (;
             dependencies, products,
             platforms=[augmented_platform],
+            script=env*script,
         ))
     end
 end
@@ -84,9 +92,9 @@ non_reg_ARGS = filter(arg -> arg != "--register", non_platform_ARGS)
 
 for (i,build) in enumerate(builds)
     build_tarballs(i == lastindex(builds) ? non_platform_ARGS : non_reg_ARGS,
-                   name, version, sources, script,
+                   name, version, sources, build.script,
                    build.platforms, build.products, build.dependencies;
-                   preferred_gcc_version=v"8", julia_compat="1.6",
+                   preferred_gcc_version=v"8", julia_compat="1.7",
                    augment_platform_block)
 end
 
